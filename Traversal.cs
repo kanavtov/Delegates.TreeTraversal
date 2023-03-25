b6c8916f44813b976855494d045356e41ba210cb
Trofimov Alexandr <kanavtov@yahoo.com>
2023-03-25 23:48:34 +0300

    refact

--- Traversal.cs
+++ Traversal.cs
@@ -8,7 +8,7 @@ namespace Delegates.TreeTraversal
 {
     public class Delegates<T1, T2>
     {
-        public Predicate<T1> ConditionAdd;
+        public Predicate<T1> EndLeaf;
         public Predicate<T1> LeftBrunch;
         public Predicate<T1> RightBrunch;
         public Func<T1, Delegates<T1, T2>, IEnumerable<T2>> LeftFunc;
@@ -20,63 +20,63 @@ namespace Delegates.TreeTraversal
 
     public static class Traversal
     {
-        static IEnumerable<T2> TraversalTree<T1, T2>(T1 node, Delegates<T1, T2> deleg)
+        static IEnumerable<T2> TraversalTree<T1, T2>(T1 node, Delegates<T1, T2> dlg)
         {
-            if (deleg.ConditionAdd(node))
-                foreach (var elem in deleg.NodeItems(node))
+            if (dlg.EndLeaf(node))
+                foreach (var elem in dlg.NodeItems(node))
                     yield return elem;
-            if (deleg.LeftBrunch(node))
-                foreach (var item in deleg.LeftItems(node))
-                    foreach (var elem in deleg.LeftFunc(item, deleg))
+            if (dlg.LeftBrunch(node))
+                foreach (var item in dlg.LeftItems(node))
+                    foreach (var elem in dlg.LeftFunc(item, dlg))
                         yield return elem;
-            if (deleg.RightBrunch(node))
-                foreach (var item in deleg.RightItems(node))
-                    foreach (var elem in deleg.RightFunc(item, deleg))
+            if (dlg.RightBrunch(node))
+                foreach (var item in dlg.RightItems(node))
+                    foreach (var elem in dlg.RightFunc(item, dlg))
                         yield return elem;
         }
 
         public static IEnumerable<Job> GetEndJobs(Job root)
         {
-            var del = new Delegates<Job, Job>();
-            del.ConditionAdd = (Job c) => (c.Subjobs.Count == 0);
-            del.LeftBrunch = (Job c) => (c.Subjobs.Count != 0);
-            del.RightBrunch = (Job c) => false;
-            del.LeftFunc = (Job c, Delegates<Job, Job> d) => TraversalTree(c, d);
-            del.RightFunc = (Job c, Delegates<Job, Job> d) => TraversalTree(c, d);
-            del.NodeItems = (Job c) => new List<Job>() { c };
-            del.LeftItems = (Job c) => c.Subjobs;
-            del.RightItems = (Job c) => new List<Job>() { c };
-            foreach (var e in TraversalTree(root, del))
+            var dlg = new Delegates<Job, Job>();
+            dlg.EndLeaf = (Job j) => (j.Subjobs.Count == 0);
+            dlg.LeftBrunch = (Job j) => (j.Subjobs.Count != 0);
+            dlg.RightBrunch = (Job j) => false;
+            dlg.LeftFunc = (Job j, Delegates<Job, Job> d) => TraversalTree(j, d);
+            dlg.RightFunc = (Job j, Delegates<Job, Job> d) => TraversalTree(j, d);
+            dlg.NodeItems = (Job j) => new List<Job>() { j };
+            dlg.LeftItems = (Job j) => j.Subjobs;
+            dlg.RightItems = (Job j) => new List<Job>();
+            foreach (var e in TraversalTree(root, dlg))
                 yield return e;
         }
 
         public static IEnumerable<Product> GetProducts(ProductCategory root)
         {
-            var del = new Delegates<ProductCategory, Product>();
-            del.ConditionAdd = (ProductCategory c) => (c.Products.Count != 0);
-            del.LeftBrunch = (ProductCategory c) => (c.Categories.Count != 0);
-            del.RightBrunch = (ProductCategory c) => false;
-            del.LeftFunc = (ProductCategory c, Delegates<ProductCategory, Product> d) => TraversalTree(c, d);
-            del.RightFunc = (ProductCategory c, Delegates<ProductCategory, Product> d) => TraversalTree(c, d);
-            del.NodeItems = (ProductCategory c) => c.Products;
-            del.LeftItems = (ProductCategory c) => c.Categories;
-            del.RightItems = (ProductCategory c) => new List<ProductCategory>() { c };
-            foreach (var e in TraversalTree(root, del))
+            var dlg = new Delegates<ProductCategory, Product>();
+            dlg.EndLeaf = (ProductCategory p) => (p.Products.Count != 0);
+            dlg.LeftBrunch = (ProductCategory p) => (p.Categories.Count != 0);
+            dlg.RightBrunch = (ProductCategory p) => false;
+            dlg.LeftFunc = (ProductCategory p, Delegates<ProductCategory, Product> d) => TraversalTree(p, d);
+            dlg.RightFunc = (ProductCategory p, Delegates<ProductCategory, Product> d) => TraversalTree(p, d);
+            dlg.NodeItems = (ProductCategory p) => p.Products;
+            dlg.LeftItems = (ProductCategory p) => p.Categories;
+            dlg.RightItems = (ProductCategory p) => new List<ProductCategory>();
+            foreach (var e in TraversalTree(root, dlg))
                 yield return e;
         }
 
         public static IEnumerable<T> GetBinaryTreeValues<T>(BinaryTree<T> root)
         {
-            var del = new Delegates<BinaryTree<T>, BinaryTree<T>>();
-            del.ConditionAdd = (BinaryTree<T> c) => (c.Left is null) & (c.Right is null);
-            del.LeftBrunch = (BinaryTree<T> c) => (c.Left != null);
-            del.RightBrunch = (BinaryTree<T> c) => (c.Right != null);
-            del.LeftFunc = (BinaryTree<T> c, Delegates<BinaryTree<T>, BinaryTree<T>> d) => TraversalTree(c.Left, d);
-            del.RightFunc = (BinaryTree<T> c, Delegates<BinaryTree<T>, BinaryTree<T>> d) => TraversalTree(c.Right, d);
-            del.NodeItems = (BinaryTree<T> c) => new List<BinaryTree<T>>() { c };
-            del.LeftItems = (BinaryTree<T> c) => new List<BinaryTree<T>>() { c };
-            del.RightItems = (BinaryTree<T> c) => new List<BinaryTree<T>>() { c }; ;
-            foreach (var e in TraversalTree(root, del))
+            var dlg = new Delegates<BinaryTree<T>, BinaryTree<T>>();
+            dlg.EndLeaf = (BinaryTree<T> b) => (b.Left is null) & (b.Right is null);
+            dlg.LeftBrunch = (BinaryTree<T> b) => (b.Left != null);
+            dlg.RightBrunch = (BinaryTree<T> b) => (b.Right != null);
+            dlg.LeftFunc = (BinaryTree<T> b, Delegates<BinaryTree<T>, BinaryTree<T>> d) => TraversalTree(b.Left, d);
+            dlg.RightFunc = (BinaryTree<T> b, Delegates<BinaryTree<T>, BinaryTree<T>> d) => TraversalTree(b.Right, d);
+            dlg.NodeItems = (BinaryTree<T> b) => new List<BinaryTree<T>>() { b };
+            dlg.LeftItems = (BinaryTree<T> b) => new List<BinaryTree<T>>() { b };
+            dlg.RightItems = (BinaryTree<T> b) => new List<BinaryTree<T>>() { b }; ;
+            foreach (var e in TraversalTree(root, dlg))
                 yield return e.Value;
         }
     }
